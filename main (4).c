8137a12ba775bffe905a2c1644bdcf84d237c134
thalipires <115191861+thalipires@users.noreply.github.com>
2022-12-01 02:10:25 -0300

    Add files via upload
    Jogo de adivinhação de número randômico

---
+++ main (4).c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void) {
+  printf("************************************\n");
+  printf("**Bem vindo ao jogo de adivinhação**\n");
+  printf("************************************\n");
+  
+  int segundos = time (0);
+  srand (segundos);
+  int numero = rand ();
+  
+  int numerosecreto = numero % 100;
+  int chute;
+  int tentativas = 1;
+  float pontos = 1000;
+  int numerodetentativas = 5;
+  int acertou = 0;
+  
+  int nivel;
+  printf("Qual o nível de dificuldade?\n");
+  printf("(1) Facil (2) Medio (3) Dificil\n\n");
+  printf ("Escolha: ");
+  scanf ("%d", &nivel);
+
+  switch (nivel){
+    case 1:
+      numerodetentativas = 20;
+      break;
+    case 2:
+      numerodetentativas = 15;
+      break;
+    default:
+      numerodetentativas = 6;
+    break;
+  }
+    
+  for (int i = 0; i <= numerodetentativas; i++){
+
+    printf("Tentativa %d\n", tentativas);
+    printf("Qual é o seu chute? ");
+    scanf("%d",&chute);
+    printf ("Seu chute foi %d\n", chute);
+
+    if (chute < 0){
+      printf("Você não pode chutar numeros negativos\n");
+      continue;
+    }
+
+    int acertou = (chute == numerosecreto);
+    int maior = chute > numerosecreto;
+    
+    if (acertou) {
+      break;
+    }
+      
+    else if (maior){
+      printf ("Seu chute foi maior que o numero secreto\n");
+      }
+    else {
+      printf ("Seu chute foi menor que o numero secreto\n");
+    }
+    tentativas ++;
+
+    float pontosperdidos = (chute - numerosecreto) / (float)2;
+    if (pontosperdidos < 0) {
+      pontosperdidos = pontosperdidos * -1;
+    }
+    pontos = pontos - pontosperdidos;
+  }
+  
+  printf("Fim de jogo\n");
+  if(acertou){
+    printf ("Parabens, você acertou!\n");
+    printf ("Voce acertou em %d tentativas", tentativas - 1);
+    printf ("Total de pontos: %.1f\n", pontos);
+  }
+
+  else{
+    printf ("Tente novamente\n");
+  }
+}
